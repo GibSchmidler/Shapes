72432e8ae93a512ddc18ebdf9bf75429a5d873d9
Gib <gibgjs@gmail.com>
2025-11-10 06:41:03 -0500

    more basic work on gameplay, setting up canvas.

--- app.py
+++ app.py
@@ -14,8 +14,23 @@ def how_to_play():
 def about():
     return render_template('about.html')
 
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
+@app.route('/vsai')
+def vsai():
+    return render_template('playing.html', player_type='AI'])
+
+@app.route('/vsplayer')
+def vsplayer():
+    return render_template('playing.html', player_type='Player')
+
+@app.route('/vsfriend')
+def vsfriend():
+    return render_template('playing.html', player_type='Friend')
 
 
 if __name__ == '__main__':
     # Listen on all interfaces so the devcontainer / host can reach it
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
---
+++ static/maingameloop.js
@@ -0,0 +1,2 @@
+let canvas = document.getElementById('game-canvas');
+let ctx = canvas.getContext('2d');
